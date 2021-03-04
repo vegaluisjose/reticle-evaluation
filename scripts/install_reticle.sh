03cb7d64c82f03d867bf9825ef4e8fc7f6bc011e
Luis Vega <vegaluisjose@gmail.com>
2021-03-04 09:33:01 -0800

    add pldi artifact

--- scripts/install_reticle.sh
+++ scripts/install_reticle.sh
@@ -13,8 +13,7 @@ pip3 install --user z3-solver numpy pandas seaborn matplotlib jupyter jupyterlab
 # install reticle
 git clone https://github.com/vegaluisjose/reticle.git
 cd reticle
-# add tag
-# git checkout $REV -b artifact 
+git checkout tags/pldi2021 -b artifact
 cargo build --release
 cargo install --bin reticle-translate --bin reticle-optimize --bin reticle-place --path .
 cd ..
